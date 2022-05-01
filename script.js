015f7077e783c896a7c25b2fe8a213662820ffcc
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-30 23:13:32 -0400

    added JS to work with DOM. Need to finish copute fucntion so it can calculate operations

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const previousOp = document.querySelector('[data-previous-operations]')
 const currentOp = document.querySelector('[data-current-operations]')
 
+
 function add(a,b){
     return a + b;
 }
@@ -22,17 +23,41 @@ function divide(a,b) {
     return a / b;
 }
 
-function operate(operator,c,d) {
+function calculate(operator,previousOp,currentOp) {
     switch(operator) {
-        case 'add':
-            return add(c,d);
-        case 'subtract':
-            return subtract(c,d);
-        case 'multiply':
-            return multiply(c,d);
-        case 'divide':
-            return divide(c,d);
+        case '+':
+            return add(previousOp,currentOp);
+        case '-':
+            return subtract(previousOp,currentOp);
+        case 'x':
+            return multiply(previousOp,currentOp);
+        case '&#247;':
+            return divide(previousOp,currentOp);
     }
 }
+
+function clear(){                       //All-clear button to clear all numbers from the sreen
+    previousOp.textContent = ''
+    currentOp.textContent = ''
+}
+allClearButton.addEventListener('click', clear)
+
+numbers.forEach(button => {
+    button.addEventListener('click', () => {
+        currentOp.textContent += button.innerText
+    })
+})
+
+equals.addEventListener('click', () => {
+    previousOp.textContent = currentOp.innerText
+    //code about currentOps being the result of the calcualtion
+    currentOp.textContent = Number(previousOp)
+})
+
+operations.forEach(operation => {
+    operation.addEventListener('click', () => {
+        currentOp.textContent += operation.innerText
+    })
+})
 
 console.log(operate('divide',3,6));
