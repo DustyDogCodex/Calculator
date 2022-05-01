a3963c15214b88c7dc4894265c83a396c30b50bd
Varun Malaviya <varunmalaviya7@gmail.com>
2022-04-30 22:32:38 -0400

    finished aesthetic changes and started work on initial JS for operations

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const numbers = document.querySelectorAll('[data-numbers]')
+const operations = document.querySelectorAll('[data-operations]')
+const equals = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOp = document.querySelector('[data-previous-operations]')
+const currentOp = document.querySelector('[data-current-operations]')
+
 function add(a,b){
     return a + b;
 }
